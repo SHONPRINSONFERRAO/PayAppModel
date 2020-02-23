3e861a7e4ea8f68ef37c4a0e891f0d6fe087bf2f
SHONPRINSONFERRAO <shonferrao@gmail.com>
2020-02-23 10:51:50 +0400

    Initial Commit

---
+++ app/src/main/java/com/shon/projects/payappmodel/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shon.projects.payappmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
